1005f57c5411590ac70ec817046ece7d59f58d96
Priyanka Sarkar <prisar13@gmail.com>
2022-05-27 22:25:59 +0530

    updated

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -9,9 +9,10 @@ export default function About() {
     <div className="head"><p>About</p>
     </div>
     <div className="bout part">
-    <div className="col-1"><p> Searching books usually means keying in an author&rsquo;s name or a book title.But if you know the name already, your search is likely to turn up books you’ve already heard about. No surprises there.</p></div>
-    <div className="col-2"><p>  How about if you didn’t need an author name at all? If you could start with what you as a reader are looking for right now? You might be in the mood for a funny read, or something to make you think. Do you want a book that’s easy going or more of a challenge to get your teeth into? 
-       </p></div>  
+    <div className="col-1"><p> Searching books usually means keying in an author&rsquo;s name or a book title. But if you know the name already, your search is likely to turn up books you’ve already heard about. No surprises there. 
+      <br/>How about if you didn’t need an author name at all? If you could start with what you as a reader are looking for right now? You might be in the mood for a funny read, or something to make you think. Do you want a book that’s easy going or more of a challenge to get your teeth into? 
+     </p> </div>
+    {/* <div className="col-2"></div>   */}
     </div>
    <div className="foot"> <Footer className="footer"/></div>
   </div>
--- src/pages/admin/admin.component.jsx
+++ src/pages/admin/admin.component.jsx
@@ -6,22 +6,40 @@ export default function About() {
   return(
   <div className="frst">
     <div className="heady"><img src="https://i.ibb.co/KwxWKG8/arr-left.png"></img><p>Admin</p><img src="https://i.ibb.co/HnKGrD9/arr-right.png"></img></div>
+    
     <div className="box">
+
     <div className="row row1">
-    <div></div><p>"I need a dog"<br/></p><div><img className="id" src="https://i.ibb.co/yh7WR9L/photo-2022-01-01-12-15-06.jpg"></img></div>
+   <div className="imag"><span><img className="id" src="https://i.ibb.co/yh7WR9L/photo-2022-01-01-12-15-06.jpg"></img></span></div> 
+   <div className="content"><p className="name">Priyanka Sarkar</p><p className="q">"I need a dog"<br/></p></div>
+   {/* <div></div> */}
    </div>
+
    <div className="row row2">
-    <div><img className="id" src="https://i.ibb.co/VtG0jDK/Whats-App-Image-2020-01-23-at-8-26-01-PM-Copy.jpg"></img></div><p>"Just put this....<br /> <strong>"I am done"</strong> "<br/></p><div>Priyanka Sarkar</div>
+    <div className="imag"><span><img className="id" src="https://i.ibb.co/VtG0jDK/Whats-App-Image-2020-01-23-at-8-26-01-PM-Copy.jpg"></img></span></div>
+    <div className="content"><p className="name">Aritra Sarkar</p><p className="q">"Just put this &gt;&gt;&gt; <br /> <strong>"I am done"</strong> "<br/></p></div>
+    {/* <div>Priyanka Sarkar</div> */}
    </div>
+
    <div className="row row3">
-    <div className="shif">Aritra Sarkar</div><p>"What I wish I could tell myself<br /> that painting and photography <br />remain a small part in my life"</p><div><img className="id" src="https://i.ibb.co/XF5KS2m/Whats-App-Image-2022-01-01-at-9-35-32-AM.jpg"></img></div>
+    <div className="imag"><span><img className="id" src="https://i.ibb.co/XF5KS2m/Whats-App-Image-2022-01-01-at-9-35-32-AM.jpg"></img></span></div>
+    <div className="content"><p className="name">Sanchari Chakraborty</p><p className="q">"What I wish is that I could tell myself<br /> that painting and photography <br />remain a small part in my life"</p></div>
+    {/* <div className="shif">Aritra Sarkar</div> */}
    </div>
+
    <div className="row row4">
-    <div><img className="id" src="https://i.ibb.co/3W6GmPz/Whats-App-Image-2022-01-01-at-9-41-38-AM.jpg"></img></div><p>I do my work at the same time <br />each day - the last minute <br /></p><div>Sanchari Chakraborty</div>
+    <div className="imag"><span><img className="id" src="https://i.ibb.co/3W6GmPz/Whats-App-Image-2022-01-01-at-9-41-38-AM.jpg"></img></span></div>
+    <div className="content"><p className="name">Sreejita Rakshit</p><p className="q">"I do my work at the same time <br />each day - the last minute."<br /></p></div>
+    {/* <div>Sanchari Chakraborty</div> */}
    </div>
-   <div className="row row5">
+
+
+
+   {/* <div className="row row5">
     <div className="last">Sreejita Rakshit</div><p><br /> <br /><br /> <br /></p><div></div>
-   </div>
+   </div> */}
+
+
    </div>
    <div className="foott"> <Footer /></div>
   </div>
